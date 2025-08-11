81a2c251f6ffe54cd4b559a9181b32880a23d540
jys555 <jumanovyashnarbek55@gmail.com>
2025-08-11 19:12:34 +0500

    Update userRoutes.js

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { upsertUser } = require('../controllers/userController');
 
+// POST /api/users -- Telegramdan kelgan user ma'lumotini saqlash
+router.post('/', upsertUser);
+
+module.exports = router;
